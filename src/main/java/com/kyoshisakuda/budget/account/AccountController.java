c9507119ed25ea15abd23cf20d411c362de96c0c
Kyoshi Sakuda <kyoshi.sakuda@gmail.com>
2019-02-22 17:50:35 -0500

    Basic crud methods to the Account object

--- src/main/java/com/kyoshisakuda/budget/account/AccountController.java
+++ src/main/java/com/kyoshisakuda/budget/account/AccountController.java
@@ -1,21 +1,39 @@
 package com.kyoshisakuda.budget.account;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class AccountController {
 
+    @Autowired
+    private AccountService accountService;
+
     @RequestMapping("/accounts")
     public List<Account> getAllAccounts() {
-        return Arrays.asList(
-                new Account(1, "Caja chica", "Efectivo"),
-                new Account(2, "Ahorros BCP", "Cuenta de Ahorros BCP"),
-                new Account(3, "Credito BCP", "Tarjeta de crédito BCP")
-        );
+        return accountService.getAllAccounts();
+    }
+
+    @RequestMapping("/accounts/{id}")
+    public Account getAccount(@PathVariable long id) {
+        return accountService.getAccount(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/accounts")
+    public void addAccount(@RequestBody Account account) {
+        accountService.addAccount(account);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/accounts/{id}")
+    public void updateAccount(@RequestBody Account account, @PathVariable long id) {
+        accountService.updateAccount(id, account);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/accounts/{id}")
+    public void deleteAccount(@PathVariable long id) {
+        accountService.deleteAccount(id);
     }
 
 }
---
+++ src/main/java/com/kyoshisakuda/budget/account/AccountService.java
@@ -0,0 +1,37 @@
+package com.kyoshisakuda.budget.account;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class AccountService {
+
+    private List<Account> accounts = new ArrayList<Account>(Arrays.asList(
+            new Account(1, "Caja chica", "Efectivo"),
+            new Account(2, "Ahorros BCP", "Cuenta de Ahorros BCP"),
+            new Account(3, "Credito BCP", "Tarjeta de crédito BCP")
+    ));
+
+    public List<Account> getAllAccounts() {
+        return accounts;
+    }
+
+    public Account getAccount(long id) {
+        return accounts.stream().filter(acc -> acc.getId() == id).findFirst().get();
+    }
+
+    public void addAccount(Account account) {
+        accounts.add(account);
+    }
+
+    public void updateAccount(long id, Account account) {
+        accounts.set((int) id - 1, account);
+    }
+
+    public void deleteAccount(long id) {
+        accounts.removeIf(acc -> acc.getId() == id);
+    }
+}
