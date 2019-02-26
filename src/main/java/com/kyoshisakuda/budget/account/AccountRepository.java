0edf45d36154fe7942060cddd2aa1e79a0a1d95c
Kyoshi Sakuda <kyoshi.sakuda@gmail.com>
2019-02-26 18:44:10 -0500

    Repository annotation added to the repository classes

--- src/main/java/com/kyoshisakuda/budget/account/AccountRepository.java
+++ src/main/java/com/kyoshisakuda/budget/account/AccountRepository.java
@@ -1,6 +1,8 @@
 package com.kyoshisakuda.budget.account;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AccountRepository extends CrudRepository<Account,Integer> {
 }
--- src/main/java/com/kyoshisakuda/budget/account/transaction/TransactionRepository.java
+++ src/main/java/com/kyoshisakuda/budget/account/transaction/TransactionRepository.java
@@ -1,9 +1,11 @@
 package com.kyoshisakuda.budget.account.transaction;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TransactionRepository extends CrudRepository<Transaction, Long> {
 
     public List<Transaction> findByAccountId(int id);
