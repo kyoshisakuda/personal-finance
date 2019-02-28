aa9ccddb6e1d016c8b6fbcc1415ce88b25cc1e51
Kyoshi Sakuda <kyoshi.sakuda@gmail.com>
2019-02-28 18:12:49 -0500

    Return type of some methods modified

--- src/main/java/com/kyoshisakuda/budget/account/AccountService.java
+++ src/main/java/com/kyoshisakuda/budget/account/AccountService.java
@@ -8,8 +8,8 @@ public interface AccountService {
 
     List<Account> getAccounts();
     Account getAccount(int id);
-    void addAccount(Account account);
-    void updateAccount(int id, Account account);
-    void deleteAccount(int id);
+    boolean addAccount(Account account);
+    boolean updateAccount(int id, Account account);
+    boolean deleteAccount(int id);
 
 }
