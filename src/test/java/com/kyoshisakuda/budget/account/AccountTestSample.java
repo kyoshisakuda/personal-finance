be18b9eba7e7d445a62a632dddde5c096df112f2
Kyoshi Sakuda <kyoshi.sakuda@gmail.com>
2019-02-28 13:46:40 -0500

    Test util class added for creation of common sample objects

---
+++ src/test/java/com/kyoshisakuda/budget/account/AccountTestSample.java
@@ -0,0 +1,33 @@
+package com.kyoshisakuda.budget.account;
+
+import com.kyoshisakuda.budget.Currency;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class AccountTestSample {
+
+    public static List<Account> getListOfSampleAccounts(boolean isEmpty) {
+        if (isEmpty)
+            return new ArrayList<>();
+        else
+            return getListOfSampleAccounts();
+    }
+
+    public static List<Account> getListOfSampleAccounts() {
+        return Stream.of(
+                new Account("AFP", "AFP Habitat", Currency.PEN),
+                new Account("Debito Soles", "BCP Cuenta Ahorros Soles", Currency.PEN),
+                new Account("Debito Dolares", "BCP Cuenta Ahorros Dolares", Currency.USD),
+                new Account("CTS", "CTS BCP", Currency.PEN),
+                new Account("Tarjeta de credito", "TC BCP", Currency.PEN))
+                .collect(Collectors.toList());
+    }
+
+    public static Account getSampleAccount() {
+        return new Account("AFP", "AFP Habitaat", Currency.PEN);
+    }
+
+}
