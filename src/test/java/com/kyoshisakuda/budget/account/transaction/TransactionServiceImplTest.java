da7721ef30ac33f41e3e7d9382841fce8f2c2e7f
Kyoshi Sakuda <kyoshi.sakuda@gmail.com>
2019-03-05 18:51:12 -0500

    Unit test for Transaction service created with its configuration

--- src/test/java/com/kyoshisakuda/budget/account/transaction/TransactionServiceImplTest.java
+++ src/test/java/com/kyoshisakuda/budget/account/transaction/TransactionServiceImplTest.java
@@ -1,7 +1,29 @@
 package com.kyoshisakuda.budget.account.transaction;
 
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
 public class TransactionServiceImplTest {
+
+    @TestConfiguration
+    static class TransactionServiceTestContextConfiguration {
+        @Bean
+        public TransactionService transactionService() {
+            return new TransactionServiceImpl();
+        }
+    }
+
+    @Autowired
+    private TransactionService service;
+
+    @MockBean
+    private TransactionRepository repository;
 
 }
